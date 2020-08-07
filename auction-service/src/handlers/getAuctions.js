0f091eefa82f2a754c8037d8d1675b4903afbd27
lazarid <idanlazar8241@gmail.com>
2020-08-07 17:22:44 +0300

    first commit

---
+++ auction-service/src/handlers/getAuctions.js
@@ -0,0 +1,28 @@
+import AWS from "aws-sdk";
+import middy from "@middy/core";
+import commonMiddleware from '../lib/commonMiddleware'
+import createError from "http-errors";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function getAuctions(event, context) {
+  let auctions;
+
+  try {
+    const results = await dynamodb
+      .scan({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+      })
+      .promise();
+     auctions = results.Items; 
+  } catch (error) {
+    console.log(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ auctions }),
+  };
+}
+export const handler = commonMiddleware(getAuctions)
---
+++ auction-service/src/handlers/placeBid.js
@@ -0,0 +1,41 @@
+import AWS from "aws-sdk";
+import commonMiddleware from "../lib/commonMiddleware";
+import createError from "http-errors";
+import { getAuctionById } from './getAuction'
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function placeBid(event, context) {
+  const { id } = event.pathParameters;
+  const { amount } = event.body;
+
+  const auction = await getAuctionById(id);
+
+  if(amount <= auction.highestBid.amount){
+      throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}`)
+  }
+
+  let updateAuction;
+
+  try {
+    const results = await dynamodb.update({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        Key: { id },
+        UpdateExpression: 'set highestBid.amount = :amount',
+        ExpressionAttributeValues: {
+          ':amount': amount,
+        },
+        ReturnValues: 'ALL_NEW',
+      }).promise();
+    updateAuction = results.Attributes;
+  } catch (error) {
+    console.log(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify( updateAuction ),
+  };
+}
+export const handler = commonMiddleware(placeBid);
